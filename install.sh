00ae91da833e46f4774a0db5a21c031f345a53a1
Mirosław Sztorc <mirek.sztorc@gmail.com>
2019-04-26 22:42:38 +0200

    install script update

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install build-essential chrpath libssl-dev libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 -y
+sudo apt-get install -y libcairo2-dev libspectre-dev librsvg2-dev libpoppler-glib-dev librsvg2-bin
+sudo apt-get install -y build-essential chrpath libssl-dev libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 -y
 sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
 sudo ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
+chmod +x src/js/psvg-convert.js
+sudo cp src/js/psvg-convert.js /usr/local/bin/psvg-convert
